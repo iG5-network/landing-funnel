b54af8709df92eaf775f245a24d37b37c29076b8
Agni Sarvaloka <agnisarvaloka@Agnis-MacBook-Air.local>
2025-07-25 03:15:31 +0530

    feat(lib):add comp,pat and lay to lib

--- src/lib/pat/head/head.tsx
+++ src/lib/pat/head/head.tsx
@@ -1,17 +1,26 @@
 import styles from './head.module.css';
 import Image from 'next/image'; 
+import Icon from '../../comp/ico/ico';
 
 
 type HeadProps = {
   brand?: string
 }
 
+const renderCtrls = 
+      <div>
+        <Icon w={32} h={32} alt='icon' src='/access.svg'/>
+        <p style={{color:'white'}}>Access</p>
+      </div>
+    
+
+
 const Head = ({brand}:HeadProps) => {
   return (
    <div className={styles.head}>
     <Image alt='brand' width={120} height={120} src={'/brand.svg'}/>
-        <button>Build</button>
-        <button>Access</button>
+    <button>Build</button>
+    {renderCtrls}
    </div>
   );
 
