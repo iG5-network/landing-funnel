579bd2c1500bd560031233071bc504fd272250d5
Agni Sarvaloka <agnisarvaloka@Agnis-MacBook-Air.local>
2025-07-25 19:59:40 +0530

    feat(head): add head ux pattern basic

--- src/lib/pat/head/head.tsx
+++ src/lib/pat/head/head.tsx
@@ -1,6 +1,7 @@
 import styles from './head.module.css';
 import Image from 'next/image'; 
 import Icon from '../../comp/ico/ico';
+import { link } from 'fs';
 
 
 type HeadProps = {
@@ -8,19 +9,46 @@ type HeadProps = {
 }
 
 const renderCtrls = 
-      <div>
+      <div className={styles.ctrl}>
         <Icon w={32} h={32} alt='icon' src='/access.svg'/>
         <p style={{color:'white'}}>Access</p>
       </div>
     
 
+    const links = [
+      {
+        name: 'Ecosystem',
+        href: '/docs'
+      },
+         {
+        name: 'Foundation',
+        href: '/build'
+      },
+      {
+        name: 'Resources',
+        href: '/blog'
+      },
+    ]
+    const renderMenu = 
+   <ul className={styles.menu}>
+   {links.map((link, index) => (
+     <li key={index}>
+       <a href={link.href}>
+         {link.name}
+       </a>
+     </li>  ))
+     }
+   </ul>
 
 const Head = ({brand}:HeadProps) => {
   return (
    <div className={styles.head}>
-    <Image alt='brand' width={120} height={120} src={'/brand.svg'}/>
-    <button>Build</button>
-    {renderCtrls}
+    <Image alt='brand' width={240} height={48} src={'/brand.svg'}/>
+    {renderMenu}
+    <div className={styles.ctrls}>
+      <button>Build</button>
+       {renderCtrls}
+    </div>
    </div>
   );
 
